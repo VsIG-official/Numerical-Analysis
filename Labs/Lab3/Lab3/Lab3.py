712e3a381fb4d7080e097a3770e3a82bb10beb5b
VsIG <valentyn2202@gmail.com>
2021-03-13 22:20:51 +0200

    ✨: add new diagonal values

--- Labs/Lab3/Lab3/Lab3.py
+++ Labs/Lab3/Lab3/Lab3.py
@@ -10,14 +10,21 @@ matrix = [[3.81, 0.25, 1.28, 2.75],
 
 rightPart = [4.21, 8.47, 2.38, 12.48]
 
-n = len(matrix)
+matrixDiagonal =    [[15.2, 4.63, 2.7, 5.03],
+                    [2.25, 6.58, 1.32, 0.49],
+                    [5.31, 0.98, 8.28, 1.04],
+                    [0, 0.119454, -0.426777, 1.48919]]
+
+rightPartDiagonal = [16.69, 8.47, 2.38, 0.026518]
+
+n = len(matrixDiagonal)
 X = [0] * n
 rounding = 6
 
 # endregion Starting Values
 
 # copy matrix to create extended matrix
-extendedMatrix = list(map(list, matrix))
+extendedMatrix = list(map(list, matrixDiagonal))
 
 # region Prints
 
@@ -60,63 +67,33 @@ def PrintAll():
 
 # add right part to main matrix
 RPCounter = 0
-while RPCounter < len(rightPart):
-    extendedMatrix[RPCounter].append(rightPart[RPCounter])
+while RPCounter < len(rightPartDiagonal):
+    extendedMatrixDiagonal[RPCounter].append(rightPartDiagonal[RPCounter])
     RPCounter += 1
 
 # region Getting rows and columns
 
-rows = len(matrix)
-columns = len(matrix)
+rows = len(matrixDiagonal)
+columns = len(matrixDiagonal)
 
-extendedRows = len(extendedMatrix)
-extendedColumns = len(extendedMatrix)+1
+extendedRows = len(extendedMatrixDiagonal)
+extendedColumns = len(extendedMatrixDiagonal)+1
 
 # endregion Getting rows and columns
 
 PrintAll()
 
-tempMatrix = list(map(list, extendedMatrix))
 
-# Iterating through matrix
-# Forward Elimination without Row Echelon
-for i in range(1):
-    for j in range(1,rows):
-        # Getting max number
-        maxNum = [max(k, key=abs) for k in zip(*tempMatrix)][0]
 
-        columnArray = [sub[i] for sub in extendedMatrix]
-
-        maxNumIndex = columnArray.index(maxNum)
-
-        # Swapping rows
-        firstTempRow = extendedMatrix[:][i]
-
-        rowToChange = extendedMatrix[:][maxNumIndex]
-
-        extendedMatrix[i] = rowToChange
-        extendedMatrix[maxNumIndex] = firstTempRow
-
-        # Doing triangular matrix
-        if matrix[j][i] == 0:continue
-        topElement = extendedMatrix[i][i]
-        bottomElement = extendedMatrix[j][i]
-
-        multiplier = topElement / bottomElement
-
-        for k in range(i,extendedColumns):
-           extendedMatrix[j][k] = round(extendedMatrix[i][k] - (extendedMatrix[j][k] * multiplier), rounding)
-
-    PrintMatrix("Extended Matrix ",extendedMatrix)
 
 
 # region Check the results
 
-# multiplied = np.round(np.dot(matrix,X),rounding)
+# multiplied = np.round(np.dot(matrixDiagonal,X),rounding)
 
 # print("Matrix multipled by X =\n",multiplied)
 
-# R = np.round(np.subtract(rightPart,multiplied),rounding)
+# R = np.round(np.subtract(rightPartDiagonal,multiplied),rounding)
 
 # print("R =\n",R)
 
