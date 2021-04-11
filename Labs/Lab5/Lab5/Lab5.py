5336cf5136c55528d6d2a43ad068bc218c406c93
VsIG <valentyn2202@gmail.com>
2021-04-11 23:02:10 +0300

    ✨: add better prints

--- Labs/Lab5/Lab5/Lab5.py
+++ Labs/Lab5/Lab5/Lab5.py
@@ -4,45 +4,56 @@ import numpy as np
 np.set_printoptions(suppress=True)
 from math import sin
 
-# Number of unknowns
-n = 3
+rounding = 5
+N = 5
 
-# Making numpy array of n & n x n size and initializing
-# to zero for storing x and y value along with differences of y
-x = np.zeros((n))
-y = np.zeros((n))
+# region Identity
+
+def Identity(N):
+    matrixForIdentity = [[0 for x in range(N)] for y in range(N)]
+    for i in range(0, N):
+        matrixForIdentity[i][i] = 1
+    return matrixForIdentity
+
+# endregion Identity
+
+# region Prints
+
+# print matrix
+def PrintMatrixAsNp(matrixName,matrix):
+    print("\n", matrixName,"=")
+    npMatrix = np.array(matrix)
+    print(npMatrix.round(rounding))
+
+# print matrix
+def PrintVectorAsNp(vectorName, vector):
+    print("\n", vectorName,"=")
+    npMatrix = np.array(vector)
+    print(npMatrix.round(rounding))
+
+# print additional parametrs
+def PrintParametrs():
+    print("\nN =", N)
+
+# endregion Prints
 
 def MySinFun(x: int, alpha=3) -> float:
     y_value = sin(alpha / 2 * x) + (x * alpha) ** (1 / 3)
     return y_value
 
-def MyNonSinFun(x: int, alpha=3) -> float:
+def MyNonSinFun(x: int) -> float:
     y_value = x ** (1. / 3.)
     return y_value
 
 #x = [3, 5, 7, 9, 11]
 #y = [MySinFun(x[0]), MySinFun(x[1]), MySinFun(x[2]), MySinFun(x[3]), MySinFun(x[4])]
 
-x = [1, 2, 3]
-y = [MyNonSinFun(x[0]), MyNonSinFun(x[1]), MyNonSinFun(x[2])]
+X_array = [1, 2, 3]
+Y_array = [MyNonSinFun(X_array[0]), MyNonSinFun(X_array[1]), MyNonSinFun(X_array[2])]
 
 # Implementing Lagrange Interpolation
-def Lagrange(xp, x, y) -> [list, list]:
-    # Set interpolated value initially to zero
-    yp = 0
-    for i in range(n):
+def Lagrange(x, y):
+    PrintVectorAsNp("X", X_array)
+    PrintVectorAsNp("Y", Y_array)
 
-        p = 1
-
-        for j in range(n):
-            if i != j:
-                p = p * (xp - x[j])/(x[i] - x[j])
-
-        yp = yp + p * y[i]
-
-        print("x =", x[i], " y =", y[i])
-    return xp, yp
-
-for i in range(n):
-    tempXP , tempYP = Lagrange(x[i], x, y)
-    print('Interpolated value at %.3f is %.3f.' % (tempXP, tempYP))
+Lagrange(X_array, Y_array)
