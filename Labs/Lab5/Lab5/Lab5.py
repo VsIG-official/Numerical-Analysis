9577d44a01b70f131d37895431f95c7f278a583c
VsIG <valentyn2202@gmail.com>
2021-04-12 01:08:20 +0300

    add some stuff

--- Labs/Lab5/Lab5/Lab5.py
+++ Labs/Lab5/Lab5/Lab5.py
@@ -7,16 +7,6 @@ from math import sin
 rounding = 5
 N = 5
 
-# region Identity
-
-def Identity(N):
-    matrixForIdentity = [[0 for x in range(N)] for y in range(N)]
-    for i in range(0, N):
-        matrixForIdentity[i][i] = 1
-    return matrixForIdentity
-
-# endregion Identity
-
 # region Prints
 
 # print matrix
@@ -41,18 +31,11 @@ def MySinFun(x: int, alpha=3) -> float:
     y_value = sin(alpha / 2 * x) + (x * alpha) ** (1 / 3)
     return y_value
 
-def MyNonSinFun(x: int) -> float:
-    y_value = x ** (1. / 3.)
-    return y_value
-
 X_array = [3, 5, 7, 9, 11]
 Y_array = [MySinFun(X_array[0]), MySinFun(X_array[1]), MySinFun(X_array[2]), MySinFun(X_array[3]), MySinFun(X_array[4])]
 
 for i in range(N):
     Y_array[i] = round(Y_array[i], rounding)
-
-#X_array = [1, 2, 3]
-#Y_array = [MyNonSinFun(X_array[0]), MyNonSinFun(X_array[1]), MyNonSinFun(X_array[2])]
 
 # Implementing Lagrange Interpolation
 def Lagrange(X_array, Y_array):
