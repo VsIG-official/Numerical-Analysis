a4f4f01ff80254c613325a3341a713c32cafee2f
VsIG <valentyn2202@gmail.com>
2021-04-12 14:52:56 +0300

    ✨: add Lagranz

--- Labs/Lab5/Lab5/Lab5.py
+++ Labs/Lab5/Lab5/Lab5.py
@@ -31,14 +31,14 @@ def MySinFun(x: int, alpha=3) -> float:
     y_value = sin(alpha / 2 * x) + (x * alpha) ** (1 / 3)
     return y_value
 
-X_array = [3, 5, 7, 9, 11]
+X_array = [1, 3, 5, 7, 9]
 Y_array = [MySinFun(X_array[0]), MySinFun(X_array[1]), MySinFun(X_array[2]), MySinFun(X_array[3]), MySinFun(X_array[4])]
 
 for i in range(N):
     Y_array[i] = round(Y_array[i], rounding)
 
 # Implementing Lagrange Interpolation
-def Lagrange(X_array, Y_array):
+def Lagrange(X_array, Y_array, element) -> float:
     PrintVectorAsNp("X", X_array)
     PrintVectorAsNp("Y", Y_array)
     firstPart = f"{Y_array[0]} * ((x - {X_array[1]})/({X_array[0]} - {X_array[1]})) * ((x - {X_array[2]})/({X_array[0]} - {X_array[2]})) * ((x - {X_array[3]})/({X_array[0]} - {X_array[3]})) * ((x - {X_array[4]})/({X_array[0]} - {X_array[4]})) +"
@@ -48,4 +48,17 @@ def Lagrange(X_array, Y_array):
     fifthPart = f"{Y_array[4]} * ((x - {X_array[0]})/({X_array[4]} - {X_array[0]})) * ((x - {X_array[1]})/({X_array[4]} - {X_array[1]})) * ((x - {X_array[2]})/({X_array[4]} - {X_array[2]})) * ((x - {X_array[3]})/({X_array[4]} - {X_array[3]}))"
     print(firstPart, secondPart, thirdPart, fourthPart, fifthPart)
 
-Lagrange(X_array, Y_array)
+    z = 0
+    for j in range(len(Y_array)):
+        p1 = 1; p2 = 1
+        for i in range(len(X_array)):
+            if i == j:
+                p1 = p1 * 1; p2 = p2 * 1
+            else:
+                p1 = p1 * (element - X_array[i])
+                p2 = p2 * (X_array[j] - X_array[i])
+        z = z + Y_array[j] * p1 / p2
+    print("Coef of",element,"element =",z)
+    return z
+
+Lagrange(X_array, Y_array, X_array[1])
