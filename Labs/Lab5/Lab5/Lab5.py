1ebdb60287b9a73a4bcaa767e2550e3757bde68a
VsIG <valentyn2202@gmail.com>
2021-04-12 17:19:24 +0300

    ✨: add better prints

--- Labs/Lab5/Lab5/Lab5.py
+++ Labs/Lab5/Lab5/Lab5.py
@@ -15,10 +15,10 @@ def PrintMatrixAsNp(matrixName,matrix):
     npMatrix = np.array(matrix)
     print(npMatrix.round(rounding))
 
-# print matrix
+# print vector
 def PrintVectorAsNp(vectorName, vector):
     print("\n", vectorName,"=")
-    npMatrix = np.array(vector)
+    npMatrix = np.array(vector).round(rounding)
     print(npMatrix.round(rounding))
 
 # print additional parametrs
@@ -27,27 +27,28 @@ def PrintParametrs():
 
 # endregion Prints
 
+# My Sin Function
 def MySinFun(x: int, alpha=3) -> float:
     y_value = sin(alpha / 2 * x) + (x * alpha) ** (1 / 3)
     return y_value
 
-X_array = [1, 3, 5, 7, 9]
+X_array = [3, 5, 7, 9, 11]
 Y_array = [MySinFun(X_array[0]), MySinFun(X_array[1]), MySinFun(X_array[2]), MySinFun(X_array[3]), MySinFun(X_array[4])]
 
-for i in range(N):
-    Y_array[i] = round(Y_array[i], rounding)
-
-# Implementing Lagrange Interpolation
-def Lagrange(X_array, Y_array, element) -> float:
+# Print Lagrange
+def PrintLagrange(X_array, Y_array):
     PrintVectorAsNp("X", X_array)
     PrintVectorAsNp("Y", Y_array)
-    firstPart = f"{Y_array[0]} * ((x - {X_array[1]})/({X_array[0]} - {X_array[1]})) * ((x - {X_array[2]})/({X_array[0]} - {X_array[2]})) * ((x - {X_array[3]})/({X_array[0]} - {X_array[3]})) * ((x - {X_array[4]})/({X_array[0]} - {X_array[4]})) +"
+    print("\nLagrange Polynom:")
+    firstPart = f"\n{Y_array[0]} * ((x - {X_array[1]})/({X_array[0]} - {X_array[1]})) * ((x - {X_array[2]})/({X_array[0]} - {X_array[2]})) * ((x - {X_array[3]})/({X_array[0]} - {X_array[3]})) * ((x - {X_array[4]})/({X_array[0]} - {X_array[4]})) +"
     secondPart = f"{Y_array[1]} * ((x - {X_array[0]})/({X_array[1]} - {X_array[0]})) * ((x - {X_array[2]})/({X_array[1]} - {X_array[2]})) * ((x - {X_array[3]})/({X_array[1]} - {X_array[3]})) * ((x - {X_array[4]})/({X_array[1]} - {X_array[4]})) +"
     thirdPart = f"{Y_array[2]} * ((x - {X_array[0]})/({X_array[2]} - {X_array[0]})) * ((x - {X_array[1]})/({X_array[2]} - {X_array[1]})) * ((x - {X_array[3]})/({X_array[2]} - {X_array[3]})) * ((x - {X_array[4]})/({X_array[2]} - {X_array[4]})) +"
     fourthPart = f"{Y_array[3]} * ((x - {X_array[0]})/({X_array[3]} - {X_array[0]})) * ((x - {X_array[1]})/({X_array[3]} - {X_array[1]})) * ((x - {X_array[2]})/({X_array[3]} - {X_array[2]})) * ((x - {X_array[4]})/({X_array[3]} - {X_array[4]})) +"
     fifthPart = f"{Y_array[4]} * ((x - {X_array[0]})/({X_array[4]} - {X_array[0]})) * ((x - {X_array[1]})/({X_array[4]} - {X_array[1]})) * ((x - {X_array[2]})/({X_array[4]} - {X_array[2]})) * ((x - {X_array[3]})/({X_array[4]} - {X_array[3]}))"
-    print(firstPart, secondPart, thirdPart, fourthPart, fifthPart)
+    print(firstPart, secondPart, thirdPart, fourthPart, fifthPart, "\n")
 
+# Implementing Lagrange Interpolation
+def Lagrange(X_array, Y_array, element) -> float:
     z = 0
     for j in range(len(Y_array)):
         p1 = 1; p2 = 1
@@ -61,4 +62,7 @@ def Lagrange(X_array, Y_array, element) -> float:
     print("Coef of",element,"element =",z)
     return z
 
-Lagrange(X_array, Y_array, X_array[1])
+PrintLagrange(X_array, Y_array)
+
+for i in range(N):
+    Lagrange(X_array, Y_array, X_array[i])
