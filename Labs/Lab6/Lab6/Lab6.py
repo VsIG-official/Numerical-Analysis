85275ce5ad36242a4bfaf36e48d40a492c0192c3
VsIG-official <valentyn2202@gmail.com>
2021-04-25 23:04:39 +0300

    ✨: add My function and derivative

--- Labs/Lab6/Lab6/Lab6.py
+++ Labs/Lab6/Lab6/Lab6.py
@@ -1,1 +1,24 @@
 
+# region Starting Values
+
+import numpy as np
+np.set_printoptions(suppress=True)
+
+rounding = 5
+epsilonValue = 0.00001
+
+# endregion Starting Values
+
+def MyFunction(x):
+    result = 10 * x ** 5 - 2 * x ** 4 - 4 * x ** 3 + 2 * x + 3
+    return result
+
+res = MyFunction(1)
+print(res)
+
+def MyPrimeFunction(x):
+    result = 50 * x ** 4 - 8 * x ** 3 - 12 * x ** 2 + 2
+    return result
+
+res = MyPrimeFunction(1)
+print(res)
