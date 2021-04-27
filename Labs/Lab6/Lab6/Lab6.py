4ae4d1a8a5d2e4a01e59a109aa877ae6cab58024
VsIG-official <valentyn2202@gmail.com>
2021-04-27 10:16:05 +0300

    🎉: finish sixth lab

--- Labs/Lab6/Lab6/Lab6.py
+++ Labs/Lab6/Lab6/Lab6.py
@@ -1,10 +1,8 @@
 # region Starting Values
 
-rounding = 5
 epsilonValue = 0.00001
 interval = [-0.8, -0.7]
 polynomEquation = "10 * x ** 5 - 2 * x ** 4 - 4 * x ** 3 + 2 * x + 3"
-polynomCoeffs = [10, -2, -4, 0, 2, 3]
 rootFromMathcad = -0.762158358336533
 half = 2
 
