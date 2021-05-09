f448f0ce703467281883ab9eb3b7b4de8a39fc08
VsIG-official <valentyn2202@gmail.com>
2021-05-09 11:54:05 +0300

    ✨: add starting value

--- Labs/Lab7/Lab7/Lab7.py
+++ Labs/Lab7/Lab7/Lab7.py
@@ -1,1 +1,17 @@
 
+# region Starting Values
+from math import cos, sin
+
+epsilonValue = 0.0001
+leftBoard = 0.1
+rightBoard = 1.1
+
+# endregion Starting Values
+
+#region Default Functions
+
+def MyFunction(x):
+    func = cos(x) / (x + 1)
+    return func
+
+#endregion Default Functions
