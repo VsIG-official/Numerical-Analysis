9e5e21096daf5e99a35c0324492bd8701fa988c9
VsIG-official <valentyn2202@gmail.com>
2021-05-11 17:43:09 +0300

    🎉: finish 7-th lab

--- Labs/Lab7/Lab7/Lab7.py
+++ Labs/Lab7/Lab7/Lab7.py
@@ -13,15 +13,11 @@ defaultNforSimpson = 1
 defaultNforGauss = 2
 rounding = 5
 
-coeffcients =  [
+valuesOfCoeffcients =  [
                [ 0.5, 2],
                [-0.577350, 0.577350, 1, 1],
                [-0.774597, 0, 0.774597, 0.555555, 0.888889, 0.555555],
-               [-0.861136, -0.339981, 0.339981, 0.861136, 0.347855, 0.652145, 0.652145, 0.347855],
-               [-0.906180, -0.538470, 0, 0.538470, 0.906180, 0.236927, 0.478629, 0.568889, 0.478629, 0.236927],
-               [-0.932470, -0.661210, -0.238620, 0.238620, 0.661210, 0.932470, 0.171324, 0.360761, 0.467914, 0.467914, 0.360761, 0.171324],
-               [-0.949108, -0.741531, -0.405845, 0, 0.405845, 0.741531, 0.949108, 0.129485, 0.279705, 0.381830, 0.417960, 0.381830, 0.279705, 0.129485],
-               [ -0.960290, -0.796666, -0.525532, -0.183434, 0.183434, 0.525532, 0.796666, 0.960290, 0.101228, 0.222381, 0.313707, 0.362684, 0.362684, 0.313707, 0.222381, 0.101228]
+               [-0.861136, -0.339981, 0.339981, 0.861136, 0.347855, 0.652145, 0.652145, 0.347855]
                ]
 
 # endregion Starting Values
@@ -65,23 +61,8 @@ def MyTwelwethPrimeFunction(x):
           (x+1)**10)+(479001600*sin(x)/(x+1)**11)+(479001600*cos(x)/(x+1)**12))/(x+1)
     return func
 
-def MyFourteenthPrimeFunction(x):
-    func=(-cos(x)+(14*sin(x)/(x+1))+(182*cos(x)/(x+1)**2)-(2184*sin(x)/(x+1)**3)-(24024*cos(x)/(x+1)**4)+(240240*sin(x)/(x+1)**5)+
-          (2162160*cos(x)/(x+1)**6)-(17297280*sin(x)/(x+1)**7)-(121080960*cos(x)/(x+1)**8)+(726485760*sin(x)/(x+1)**9)+
-          (3632428800*cos(x)/(x+1)**10)-(14529715200*sin(x)/(x+1)**11)-(43589145600*cos(x)/(x+1)**12)+(87178291200*sin(x)/(x+1)**13)
-          +(87178291200*cos(x)/(x+1)**14))/(x+1)
-    return func
-
-def MySixteenthPrimeFunction(x):
-    func=(cos(x)-(16*sin(x)/(x+1))-(240*cos(x)/(x+1)**2)+(3360*sin(x)/(x+1)**3)+(43680*cos(x)/(x+1)**4)-(524160*sin(x)/(x+1)**5)-
-          (5765760*cos(x)/(x+1)**6)+(57657600*sin(x)/(x+1)**7)+(518918400*cos(x)/(x+1)**8)-(4151347200*sin(x)/(x+1)**9)-
-          (29059430400*cos(x)/(x+1)**10)+(174356582400*sin(x)/(x+1)**11)+(871782912000*cos(x)/(x+1)**12)-(3487131648000*cos(x)/(x+1)**13)
-          +(10461394944000*cos(x)/(x+1)**14)+(20922789888000*cos(x)/(x+1)**15)+(20922789888000*cos(x)/(x+1)**16))/(x+1)
-    return func
-
 SpecificFunctionsForTheGauss = [MyFourthPrimeFunction, MySixthPrimeFunction, MyEigthPrimeFunction,
-                                MyTenthPrimeFunction, MyTwelwethPrimeFunction,
-                                MyFourteenthPrimeFunction, MySixteenthPrimeFunction]
+                                MyTenthPrimeFunction, MyTwelwethPrimeFunction]
 
 #endregion Default Functions
 
@@ -117,8 +98,8 @@ def Gauss(firstInterval, secondInterval):
     analyzeDifference, Nvalue = GaussDifference(firstInterval, secondInterval, defaultNforGauss)
     tempResult = 0
     for z in range(Nvalue):
-        tempIndex = int(((len(coeffcients[Nvalue])/2)+z)-1)
-        tempResult = tempResult + coeffcients[Nvalue-1][tempIndex] * ReverseMyFunction(coeffcients[Nvalue-1][z])
+        tempIndex = int(((len(valuesOfCoeffcients[Nvalue])/2)+z)-1)
+        tempResult = tempResult + valuesOfCoeffcients[Nvalue-1][tempIndex] * ReverseMyFunction(valuesOfCoeffcients[Nvalue-1][z])
     finalResult = ((secondInterval - firstInterval) / 2) * tempResult
 
     return finalResult, analyzeDifference, Nvalue
